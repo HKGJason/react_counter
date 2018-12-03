876591f70377ac2b235e9a36775fad64f12f2c59
J <jason19951020@gmail.com>
2018-12-03 18:54:59 +0800

    set default number

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-	state = {number: 0}
+	state = {number: this.props.number}
   render() {
     return (
       <div>
-        <button onClick={()=>{this.setState({number: this.state.number+1})}}>Hello world!</button>
+        <button>Hello world!</button>
         <span>number: {this.state.number}</span>
 	  </div>
     );
