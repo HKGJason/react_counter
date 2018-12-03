d2a9497826b44361b7b9fa6db36d521e1bf47abe
J <jason19951020@gmail.com>
2018-12-03 22:05:03 +0800

    change into add minus number button;

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Number from './components/Number.js';
-import Line from './components/Line.js';
 
 class App extends Component {
-	
-	updateNum = () =>{
-		let newNum = this.props.onClickWho(this.state.number)
-		this.setState({number: newNum})
-	}
-	
-	state = {number: 1}
+
+  state={number: 0}
   render() {
     return (
       <div>
-        <button onClick={this.updateNum}>Hello world!</button>
-		<Number /><span>{this.state.number}</span>
-	    {Array(this.state.number).fill(1).map(() =><Line/>)}
-	  </div>
+        <button onClick={()=>{this.setState({number: this.state.number+1})}}>+</button>
+        <span>{this.state.number}</span>
+        <button onClick={()=>{this.setState({number: this.state.number-1})}}>-</button>
+      </div>
     );
   }
 }
